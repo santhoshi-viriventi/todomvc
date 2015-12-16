a30ef0f642640509bd2e434aa810a77b10cb620c
santhoshi-viriventi <santhoshi.viriventi@capgemini.com>
2015-12-16 12:04:16 +0530

    Update karma.conf.js

--- examples/angularjs/test/config/karma.conf.js
+++ examples/angularjs/test/config/karma.conf.js
@@ -1,22 +1,21 @@
 module.exports = function (config) {
-	'use strict';
+                'use strict';
 
-	config.set({
-		basePath: '../../',
-		frameworks: ['jasmine'],
-		files: [
-			'node_modules/angular/angular.js',
-			'node_modules/angular-route/angular-route.js',
-			'node_modules/angular-mocks/angular-mocks.js',
-			'js/**/*.js',
-			'test/unit/**/*.js'
-		],
-		autoWatch: true,
-		singleRun: false,
-		browsers: ['Chrome', 'Firefox'],
-		
-		reporters: ['coverage','junit'],
-		coverageReporter: {
+                config.set({
+                                basePath: '../../',
+                                frameworks: ['jasmine'],
+                                files: [
+                                                'node_modules/angular/angular.js',
+                                                'node_modules/angular-route/angular-route.js',
+                                                'node_modules/angular-mocks/angular-mocks.js',
+                                                'js/**/*.js',
+                                                'test/unit/**/*.js'
+                                ],
+                                autoWatch: true,
+                                singleRun: false,
+                                browsers: ['Chrome', 'Firefox'],                 
+                                reporters: ['coverage','junit'],
+                                coverageReporter: {
             dir: 'test/coverage/',
             reporters: [
                 {
@@ -26,16 +25,10 @@ module.exports = function (config) {
                     type: 'cobertura',subdir: '.', file: 'cobertura.xml'
                 }
             ]
-            }
-            
-            junitReporter: {
-            	
-            		outputfile: 'test/coverage/test-results.xml'
-            		}
-            	
-
-            
-		
-	});
+        },
+                                junitReporter : {
+                                  outputFile: 'test/coverage/test-results.xml'
+                                }
+                                
+                });
 };
-
